d894f0aaf8cad0c7579bab43eb32ac22287b0c1d
Brais Suarez <brais.suarez@mediamonks.com>
2019-12-17 15:21:14 +0100

    Updates

---
+++ precache-manifest.9bc852b3118788d9a499b0ccabe8c40b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98e61e3e984627f5a96655b5e7dd2341",
+    "url": "/ts-bitcoin-app/index.html"
+  },
+  {
+    "revision": "66bdbea777017b5bd6ce",
+    "url": "/ts-bitcoin-app/static/css/main.bcec0998.chunk.css"
+  },
+  {
+    "revision": "27526c45585e326d5e04",
+    "url": "/ts-bitcoin-app/static/js/2.39ad9213.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/ts-bitcoin-app/static/js/2.39ad9213.chunk.js.LICENSE"
+  },
+  {
+    "revision": "66bdbea777017b5bd6ce",
+    "url": "/ts-bitcoin-app/static/js/main.7450c455.chunk.js"
+  },
+  {
+    "revision": "34eab6c2ee9b689bf5e0",
+    "url": "/ts-bitcoin-app/static/js/runtime-main.4ed3665c.js"
+  },
+  {
+    "revision": "8ee75c23a1c738efe8452e80afdd8365",
+    "url": "/ts-bitcoin-app/static/media/bitcoinLogo.8ee75c23.png"
+  }
+]);
